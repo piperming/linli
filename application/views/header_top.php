446e230bee05eb1a440fb65794bbd1d2c7108d17
xnnye <xnnye@qq.com>
2013-04-26 00:32:59 +0800

    jquery UI添加

--- application/views/header_top.php
+++ application/views/header_top.php
@@ -4,8 +4,11 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>交通银行信用卡管理系统</title>
     <link rel="stylesheet" href="/html/css/style.css">
+    <link rel="stylesheet" href="/html/css/jquery-ui.css">
     <script type="text/javascript" src="/html/js/jquery-1.6.js"></script>
-    <script type="text/javascript" src="/html/js/base.js"></script>
+    <script type="text/javascript" src="/html/js/jquery.tmpl.mini.js"></script>
+    <script type="text/javascript" src="/html/js/jquery.blockUI.js"></script>
+    <script type="text/javascript" src="/html/js/jquery-ui-1.7.2.custom.mini.js"></script>
     <script type="text/javascript" src="/html/js/base.js"></script>
 </head>
 <body>
--- application/views/staff/staff_list.php
+++ application/views/staff/staff_list.php
@@ -18,28 +18,66 @@
     <tbody>
     <?php foreach($staffs as $staff):?>
     <tr>
-        <td class="center"><?php echo $staff['id'];?></td>
-        <td class="center"><?php echo $staff['staff_number'];?></td>
-        <td class="center"><?php echo $staff['staff_name'];?></td>
-        <td class="center"><?php echo $staff['sex']==0?'男':'女';?></td>
-        <td class="center"><?php echo date('Y年m月d日' , $staff['brithday']);?></td>
-        <td class="center"><?php echo $staff['email'];?></td>
-        <td class="center"><?php echo $staff['education'];?></td>
-        <td class="center"><?php echo $staff['phone'];?></td>
-        <td class="center"><?php echo $staff['tel'];?></td>
-        <td class="center"><?php echo date('Y-m-d H:i:s' , $staff['ctime']);?></td>
-        <td class="center"><a href="javascript:void(0)" class="edit_btn">编辑</a><a href="javascript:void(0)" sid="<?php echo $staff['id']?>" class="del_btn">删除</a></td>
+        <td class="center row_<?=$staff['id']?>"><?=$staff['id'];?></td>
+        <td class="center row_<?=$staff['id']?>"><?=$staff['staff_number'];?></td>
+        <td class="center row_<?=$staff['id']?>"><?=$staff['staff_name'];?></td>
+        <td class="center row_<?=$staff['id']?>"><?= $staff['sex']==0?'男':'女';?></td>
+        <td class="center row_<?=$staff['id']?>"><?= date('Y年m月d日' , $staff['brithday']);?></td>
+        <td class="center row_<?=$staff['id']?>"><?= $staff['email'];?></td>
+        <td class="center row_<?=$staff['id']?>"><?= $staff['education'];?></td>
+        <td class="center row_<?=$staff['id']?>"><?= $staff['phone'];?></td>
+        <td class="center row_<?=$staff['id']?>"><?= $staff['tel'];?></td>
+        <td class="center row_<?=$staff['id']?>"><?= date('Y-m-d H:i:s' , $staff['ctime']);?></td>
+        <td class="center row_<?=$staff['id']?>"><a href="javascript:void(0)" class="edit_btn" sid="<?= $staff['id']?>">编辑</a><a href="javascript:void(0)" sid="<?php echo $staff['id']?>" class="del_btn">删除</a></td>
     </tr>
     <?php endforeach?>
 </tbody>
 </table>
 <script id="editTemplate" type="text/x-jquery-tmpl">
-   <h5>编辑</h5>
-    ID:${id}
+    <table id="edit_table">
+        <tr><td width="120">ID</td><td width="260">${id}</td></tr>
+        <tr><td>员工工号</td><td>${staff_number}</td></tr>
+        <tr><td>员工姓名</td><td>${staff_name}</td></tr>
+        <tr><td>性别</td><td>${sex}</td></tr>
+        <tr><td>生日</td><td>${brithday}</td></tr>
+        <tr><td>邮箱</td><td><input type="text" name="" value="${email}"/></td></tr>
+        <tr><td>教育程度</td><td><input type="text" name="" value="${education}"/></td></tr>
+        <tr><td>手机号码</td><td><input type="text" name="" value="${phone}"/></td></tr>
+        <tr><td>座机号码</td><td><input type="text" name="" value="${tel}"/></td></tr>
+        <tr><td>注册时间</td><td>${ctime}</td></tr>
+    </table>
 </script>
 <script type="text/javascript">
     $('.edit_btn').click(function(){
-
+        var json = Object();
+        var key_arr = Array('id' , 'staff_number' , 'staff_name' , 'sex' , 'brithday' , 'email' , 'education' , 'phone' , 'tel' , 'ctime');
+        var id = $(this).attr('sid');
+        $('.row_'+id).each(function(i){
+            var key = key_arr[i];
+            json[key] = $(this).text();
+        });
+        var content =  $('#editTemplate').tmpl(json);
+//        $.dialog({
+//            position: ['center', 70],
+//            resizable: false,
+//            draggable: false,
+//            title: '编辑员工信息',
+//            modal: true,
+//            width: 850,
+//            height:700,
+//            minWidth: 700,
+//            maxWidth: 800
+//        });
+        $.blockUI({
+            theme:true,
+            title:'编辑员工信息',
+            message:content,
+            themedCSS:{
+                width:'auto',
+                height:'auto',
+                top:'50px'
+            }
+        });
     });
 </script>
 <script type="text/javascript">
